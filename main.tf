f6f77a5c8fda8f91a8de8f4d841da774ae6f5643
KKHARA1 <52095270+KKHARA1@users.noreply.github.com>
2025-08-01 20:35:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+backend "azurerm" {
+    resource_group_name  = "backendrg"          # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "kkbackstg "                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "kkcont"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "pipeline.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
